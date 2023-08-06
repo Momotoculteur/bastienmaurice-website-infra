bf4b0911a8b8f0b96a65dd4b6c42abdb5e342c0a
Bastien MAURICE <bastien_maurice@hotmail.com>
2023-08-07 00:30:49 +0200

    ;

--- route53.tf
+++ route53.tf
@@ -11,8 +11,8 @@ resource "aws_route53_record" "r53_record" {
   name    = var.domain_name
   type    = "A"
   alias {
-    name                   = aws_s3_bucket.root_bucket.website_endpoint
+    name                   = aws_s3_bucket_website_configuration.config_root.website_domain
     zone_id                = aws_s3_bucket.root_bucket.hosted_zone_id
-    evaluate_target_health = ""
+    evaluate_target_health = true
   }
 }
