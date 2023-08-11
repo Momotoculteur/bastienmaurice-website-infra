7d0f342aeda27a848929954512eaf747e01e97e4
Bastien MAURICE <b.maurice@betclicgroup.com>
2023-08-11 23:19:16 +0200

    add row

--- route53.tf
+++ route53.tf
@@ -6,10 +6,23 @@ resource "aws_route53_zone" "r53_zone" {
   }
 }
 
-resource "aws_route53_record" "r53_record" {
+resource "aws_route53_record" "r53_record_root" {
   zone_id = aws_route53_zone.r53_zone.id
   name    = var.domain_name
   type    = "A"
+
+  alias {
+    name                   = aws_s3_bucket_website_configuration.config_root.website_domain
+    zone_id                = aws_s3_bucket.root_bucket.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "r53_record_www" {
+  zone_id = aws_route53_zone.r53_zone.id
+  name    = "www.${var.domain_name}"
+  type    = "A"
+
   alias {
     name                   = aws_s3_bucket_website_configuration.config_root.website_domain
     zone_id                = aws_s3_bucket.root_bucket.hosted_zone_id
