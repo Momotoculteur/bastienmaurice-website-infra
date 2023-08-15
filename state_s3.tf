fdc5ad0dfdd019316c374a3010afc80196e37ba8
Bastien MAURICE <bastien_maurice@hotmail.com>
2023-08-15 16:34:22 +0200

    delete useless policy

--- state_s3.tf
+++ state_s3.tf
@@ -25,31 +25,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encryption" {
   }
 }
 
-
-/*
-resource "aws_s3_bucket_policy" "policy_s3_state" {
-  bucket = aws_s3_bucket.s3_state.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Principal = "*"
-        "Effect" : "Allow",
-        "Action" : "s3:ListBucket",
-        "Resource" : aws_s3_bucket.s3_state.arn
-      },
-      {
-        Principal = "*"
-        "Effect" : "Allow",
-        "Action" : ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
-        "Resource" : [aws_s3_bucket.s3_state.arn, "${aws_s3_bucket.s3_state.arn}/*"]
-      }
-    ]
-  })
-}
-*/
-
 resource "aws_s3_bucket_public_access_block" "s3_access" {
   bucket = aws_s3_bucket.s3_state.id
 
